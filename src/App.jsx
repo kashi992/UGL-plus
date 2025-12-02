27765e4b3cc3fa9e28afd2ee7b06386956a0c95b
kashi992 <kashifrajput992@gmail.com>
2025-12-02 14:28:13 +0500

    Initial UGL visualizer setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+  <div className="min-h-screen flex items-center justify-center bg-red-900">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
