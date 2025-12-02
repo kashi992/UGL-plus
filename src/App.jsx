6a9d736a72d8df2dec26bd96e755e0dc4afa0898
kashi992 <kashifrajput992@gmail.com>
2025-12-02 14:31:18 +0500

    test4

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
   <div className="min-h-screen flex items-center justify-center bg-red-900">
       <h1 className="text-4xl font-bold text-white">
-        Tailwind is working 🚀
+        Tailwind is working- test 🚀
       </h1>
     </div>
   )
